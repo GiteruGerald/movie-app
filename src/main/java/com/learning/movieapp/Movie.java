734665b802bf8c2fe77812d3d7acace30ae5cfab
GerryNdungu <gerryjerry36@gmail.com>
2022-08-30 21:22:49 +0300

    Initial commit

---
+++ src/main/java/com/learning/movieapp/Movie.java
@@ -0,0 +1,10 @@
+package com.learning.movieapp;
+
+public class Movie {
+    private Long id;
+    private String name;
+    private String type;
+    private Long duration;
+    private Long releaseYear;
+
+}
